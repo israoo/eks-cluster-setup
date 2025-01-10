76e4472836033c48e6ecb5689826eefb03208cfb
Israel Oña Ordoñez <israoo@outlook.com>
2025-01-10 17:36:10 -0500

    refactor(outputs.tf): rename output variable from eks_cluster_ca_data to eks_cluster_name for clarity and consistency

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "eks_cluster_ca_data" {
-  description = "The certificate-authority-data for the EKS Kubernetes."
-  value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
+output "eks_cluster_name" {
+  description = "The name of the EKS Kubernetes."
+  value = aws_eks_cluster.eks_cluster.name
 }
